c6984099d1704caa2c7e76aa39448bbe59e18e61
noobylub <73588764+noobylub@users.noreply.github.com>
2021-02-08 14:57:56 +0200

    Intialize array list

--- src/com/company/ArrayList.java
+++ src/com/company/ArrayList.java
@@ -1,7 +1,24 @@
 
 package com.company;
-class  ArrayList{
+//THe way to import an array list
+import  java.util.ArrayList;
+
+class  arrayList{
     public static void main(String[] args){
-        System.out.println("hello");
+        //arraylist is mutable, it can be changed
+        //First import the arraylist, the imported class like a scanner
+
+
+        //then we define it and initialize it
+        //The type of arraylist is within the <> bracket
+        //This is an empty list
+        ArrayList<Integer> firstArrayList = new ArrayList<Integer>();
+        //Like javascript, you put it values
+        firstArrayList.add(56);
+        firstArrayList.add(65);
+        for(int x:firstArrayList){
+            System.out.println(x);
+        }
+
     }
 }
